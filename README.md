0587d2bd4bbbe27b7a7f399933d94b3b8e6df55b
Christian Mutti <chrsmutti@gmail.com>
2018-07-22 21:24:05 -0300

    Added 0x8XXX codes and some api enhancements

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Chip8
 
 Rust implementation of a CHIP-8 VM.
+
+### Opcodes
+
+- **8XY0**: Sets VX to the value of VY.
+- **8XY1**: Sets VX to VX or VY. (Bitwise OR operation)
+- **8XY2**: Sets VX to VX and VY. (Bitwise AND operation)
+- **8XY3**: Sets VX to VX xor VY.
+- **8XY4**: Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.
+- **8XY5**: VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
+- **8XY6**: Shifts VY right by one and stores the result to VX (VY remains unchanged). VF is set to the value of the least significant bit of VY before the shift.
+- **8XY7**: Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
+- **8XYE**: Shifts VY left by one and copies the result to VX. VF is set to the value of the most significant bit of VY before the shift.
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,10 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::fs::File;
 use std::io::Read;
-use std::u16;
 
-const SCALE: u32 = 5;
-const WIDTH: u32 = 64 * SCALE;
-const HEIGHT: u32 = 32 * SCALE;
+const WIDTH: u8 = 64;
+const HEIGHT: u8 = 32;
+const GFX_SIZE: usize = (HEIGHT as usize) * (WIDTH as usize);
 
 fn main() {
     let mut buf = [0; 4096];
@@ -19,8 +18,12 @@ fn main() {
     let context = sdl2::init().expect("Failed to initialize SDL2.");
     let video_subsystem = context.video().expect("Failed to aquire video subsystem.");
 
+    let scale = 5u32;
+    let window_width = (WIDTH as u32) * scale;
+    let window_height = (HEIGHT as u32) * scale;
+
     let _window = video_subsystem
-        .window("Chip8", WIDTH, HEIGHT)
+        .window("Chip8", window_width, window_height)
         .position_centered()
         .build()
         .expect("Failed to create window.");
@@ -49,29 +52,99 @@ fn main() {
 }
 
 #[allow(dead_code)]
-#[derive(Debug)]
 struct Processor {
     pub pc: usize,
     i: u16,
-    memory: Vec<u8>,
+    memory: [u8; 4096],
+    v: [u8; 16],
+    stack: [u16; 16],
+    sp: usize,
+    key: [u8; 16],
+    gfx: [u8; GFX_SIZE],
 }
 
 impl Processor {
     fn new(memory: [u8; 4096]) -> Self {
         Processor {
+            memory,
             pc: 0x200,
             i: 0x0,
-            memory: memory.to_vec(),
+            v: [0x0; 16],
+            stack: [0x0; 16],
+            sp: 0x0,
+            key: [0x0; 16],
+            gfx: [0x0; GFX_SIZE],
         }
     }
 
     fn execute_cycle(&mut self) {
         let opcode = self.current_opcode();
 
-        println!("Current Opcode: {:x}", opcode);
+        match opcode[0] {
+            0x8 => self.eight(opcode),
+            _ => {}
+        }
     }
 
-    fn current_opcode(&self) -> u16 {
-        (u16::from(self.memory[self.pc]) << 8) | u16::from(self.memory[self.pc + 1])
+    /// Implementation of the 0x8 Opcodes.
+    fn eight(&mut self, opcode: [u8; 4]) {
+        let x = usize::from(opcode[1]);
+        let y = usize::from(opcode[2]);
+
+        match opcode[3] {
+            0x0 => self.v[x] = self.v[y],
+            0x1 => self.v[x] |= self.v[y],
+            0x2 => self.v[x] &= self.v[y],
+            0x3 => self.v[x] ^= self.v[y],
+            0x4 => {
+                self.v[x] = match self.v[x].overflowing_add(self.v[y]) {
+                    (result, true) => {
+                        self.v[0xF] = 0x1;
+                        result
+                    }
+                    (result, false) => result,
+                }
+            }
+            0x5 => {
+                self.v[x] = match self.v[x].overflowing_sub(self.v[y]) {
+                    (result, true) => {
+                        self.v[0xF] = 0x1;
+                        result
+                    }
+                    (result, false) => result,
+                }
+            }
+            0x6 => {
+                self.v[0xF] = self.v[y] & 0x0F;
+                self.v[x] = self.v[y] >> 1;
+            }
+            0x7 => {
+                self.v[x] = match self.v[y].overflowing_sub(self.v[x]) {
+                    (result, true) => {
+                        self.v[0xF] = 0x1;
+                        result
+                    }
+                    (result, false) => result,
+                }
+            }
+            0xE => {
+                self.v[0xF] = self.v[y] & 0xF0;
+                self.v[y] <<= 1;
+                self.v[x] = self.v[y];
+            }
+
+            _ => unreachable!(),
+        }
+    }
+
+    fn current_opcode(&self) -> [u8; 4] {
+        let code = (u16::from(self.memory[self.pc]) << 8) | u16::from(self.memory[self.pc + 1]);
+
+        [
+            ((code & 0xF000) >> 12) as u8,
+            ((code & 0x0F00) >> 8) as u8,
+            ((code & 0x00F0) >> 4) as u8,
+            ((code & 0x000F)) as u8,
+        ]
     }
 }
